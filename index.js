ae51657d61f9ee030542b0dcd207243cc684ea36
Evan Xd <itoyxd@gmail.com>
2013-11-10 00:39:47 +0800

    ..@_@|||||.. - Start the http and websocket server with one command.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express'),
+    childProcess = require('child_process'),
     app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.listen(8080);
+
+// Start the websocket server.
+childProcess.spawn('node', ['./ws_server/index']);
