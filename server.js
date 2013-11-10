268e6f4db2def6f6e249b8aa16e721495abb21a4
Yuren Ju <yurenju@gmail.com>
2013-11-10 21:04:02 +0800

    move index.js to server.js

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('sx83ZcpGo9-B20-Z');
 
-var isProduction = (process.env.NODE_ENV === 'production');
-var http = require('http');
-var port = (isProduction ? 80 : 8000);
+const IS_PRODUCTION = true;
+var express = require('express'),
+    childProcess = require('child_process'),
+    app = express();
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/mozart" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+app.use(express.static(__dirname + '/public'));
+app.listen(80);
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
-  if (err) { console.error(err); process.exit(-1); }
+// Start the websocket server.
+if (IS_PRODUCTION) {
+  childProcess.spawn('node', ['./ws_server/index']);
+}
 
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
-    });
-  }
 
-  console.log('Server running at http://0.0.0.0:' + port + '/');
-});
+// var isProduction = (process.env.NODE_ENV === 'production');
+// var http = require('http');
+// var port = (isProduction ? 80 : 8000);
+
+// http.createServer(function (req, res) {
+//   // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
+//   var voteko = '<iframe src="http://nodeknockout.com/iframe/mozart" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('<html><body>' + voteko + '</body></html>\n');
+// }).listen(port, function(err) {
+//   if (err) { console.error(err); process.exit(-1); }
+
+//   // if run as root, downgrade to the owner of this file
+//   if (process.getuid() === 0) {
+//     require('fs').stat(__filename, function(err, stats) {
+//       if (err) { return console.error(err); }
+//       process.setuid(stats.uid);
+//     });
+//   }
+
+//   console.log('Server running at http://0.0.0.0:' + port + '/');
+// });
