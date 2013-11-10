a7740a9e4e51aace0e4b497b53058d05bf1e7a47
Rex Lee <rexboy@mozilla.com>
2013-11-10 21:10:51 +0800

    Remove unused demo song

--- ws_server/lib/tracks_manifest.js
+++ ws_server/lib/tracks_manifest.js
@@ -1,20 +1,5 @@
 var TracksManifest = {
   data: {
-    'la_campanella_pi.mid': {
-      '01': ['acoustic_grand_piano'],
-      '02': ['acoustic_grand_piano'],
-      '03': ['acoustic_grand_piano']
-    },
-
-    'wedding.mid': {
-      '01': ['acoustic_grand_piano'],
-      '02': ['bright_acoustic_piano'],
-      '03': ['string_ensemble_1'],
-      '04': ['pad_4_choir'],
-      '05': ['harmonica'],
-      '06': ['fx_1_rain']
-    },
-
     'pachelbel_canon.mid': {
       '01': ['violin'],
       '02': ['violin'],
@@ -29,21 +14,6 @@ var TracksManifest = {
 
     'turkish_match.mid': {
       '01': ['acoustic_grand_piano']
-    },
-
-    'StarWars2.mid': {
-      '01': ['flute'],
-      '02': ['bassoon'],
-      '03': ['french_horn'],
-      '04': ['trumpet'],
-      '05': ['trombone'],
-      '06': ['timpani'],
-      '07': ['orchestral_harp'],
-      '08': ['bright_acoustic_piano'],
-      '09': ['string_ensemble_1'],
-      '10': ['synth_drum'],
-      '11': ['string_ensemble_1'],
-      '12': ['tremolo_strings']
     },
 
     'symphony_of_destiny.mid': {
