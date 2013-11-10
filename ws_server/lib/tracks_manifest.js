17fe230c29d89059ecd7de9fd31bc4026cbca57c
Rex Lee <rexboy@mozilla.com>
2013-11-10 16:21:23 +0800

    update track manifest for starwars

--- ws_server/lib/tracks_manifest.js
+++ ws_server/lib/tracks_manifest.js
@@ -8,7 +8,7 @@ var TracksManifest = {
 
     'wedding.mid': {
       '01': ['acoustic_grand_piano'],
-      '02': ['acoustic_grand_piano'],
+      '02': ['bright_acoustic_piano'],
       '03': ['string_ensemble_1'],
       '04': ['pad_4_choir'],
       '05': ['harmonica'],
@@ -32,12 +32,18 @@ var TracksManifest = {
     },
 
     'StarWars2.mid': {
-      '01': ['acoustic_grand_piano'],
-      '02': ['acoustic_grand_piano'],
-      '03': ['acoustic_grand_piano'],
-      '04': ['acoustic_grand_piano'],
-      '05': ['acoustic_grand_piano'],
-      '06': ['acoustic_grand_piano'],
+      '01': ['flute'],
+      '02': ['bassoon'],
+      '03': ['french_horn'],
+      '04': ['trumpet'],
+      '05': ['trombone'],
+      '06': ['timpani'],
+      '07': ['orchestral_harp'],
+      '08': ['bright_acoustic_piano'],
+      '09': ['string_ensemble_1'],
+      '10': ['synth_drum'],
+      '11': ['string_ensemble_1'],
+      '12': ['tremolo_strings']
     }
   }
 };
