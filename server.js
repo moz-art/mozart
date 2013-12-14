fd21477fb293f6a24fc2c1000da7fd616a70503b
John <huchengtw@gmail.com>
2013-12-14 15:47:55 +0800

    remove useless codes.

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 
 const IS_PRODUCTION = true;
 var express = require('express'),
@@ -6,34 +5,14 @@ var express = require('express'),
     app = express(),
     WebSocketServer = require('./ws_server/lib/ws_server');
 
+var port = (IS_PRODUCTION ? 80 : 8000);
+
 app.use(express.static(__dirname + '/public'));
 var server = http.createServer(app);
-server.listen(80);
+server.listen(port);
 
 // start websocket server
 wsServer = new WebSocketServer(null, server);
 
+console.log('Server running at http://127.0.0.1:' + port + '/');
 
-// var isProduction = (process.env.NODE_ENV === 'production');
-// var http = require('http');
-// var port = (isProduction ? 80 : 8000);
-
-// http.createServer(function (req, res) {
-//   // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-//   var voteko = '<iframe src="http://nodeknockout.com/iframe/mozart" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
-
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   res.end('<html><body>' + voteko + '</body></html>\n');
-// }).listen(port, function(err) {
-//   if (err) { console.error(err); process.exit(-1); }
-
-//   // if run as root, downgrade to the owner of this file
-//   if (process.getuid() === 0) {
-//     require('fs').stat(__filename, function(err, stats) {
-//       if (err) { return console.error(err); }
-//       process.setuid(stats.uid);
-//     });
-//   }
-
-//   console.log('Server running at http://0.0.0.0:' + port + '/');
-// });
