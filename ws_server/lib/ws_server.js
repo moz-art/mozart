ac75831aff980f61c728f0a38d71e8cf8d200cda
John Hu <im@john.hu>
2018-02-26 12:08:08 +0800

    rewrite some code to use class and use 6.0.0 as the base node version

--- ws_server/lib/ws_server.js
+++ ws_server/lib/ws_server.js
@@ -1,72 +1,71 @@
 const DEFAULT_PORT = 8888,
       READY_STATE_OPEN = 1;
-var Server = require('ws').Server,
-    Hashids = require('hashids'),
-    uuid = require('node-uuid'),
-    SocketHandler = require('./socket_handler'),
-    socketHandler;
+const Server = require('ws').Server;
+const Hashids = require('hashids');
+const uuid = require('node-uuid');
+const SocketHandler = require('./socket_handler');
 
-function WebSocketServer(port, server) {
-  this.port = port || DEFAULT_PORT;
-  this.server = server ? new Server({server: server}) : 
-                         new Server({ port: this.port });
-  this.groups = {};
-  socketHandler = new SocketHandler(this);
-  this._init();
-}
+class WebSocketServer {
+  constructor(port, server) {
+    this.port = port || DEFAULT_PORT;
+    this.server = server ? new Server({server: server}) :
+                          new Server({ port: this.port });
+    this.groups = {};
+    this.socketHandler = new SocketHandler(this);
+    this._init();
+  }
 
-WebSocketServer.prototype = {
-  _init: function() {
-    this.server.on('connection', function(client) {
+  _init() {
+    this.server.on('connection', (client) => {
       // Setup a unique id for the client.
       client.id = this.generateHashId();
       console.log('The client ' + client.id + ' is connected.');
 
-      client.on('message', function(data) {
-        socketHandler.handle(client, JSON.parse(data));
-      }.bind(this));
+      client.on('message', (data) => {
+        this.socketHandler.handle(client, JSON.parse(data));
+      });
 
-      client.on('close', function() {
+      client.on('close', () => {
         // DOTO: remove the client from the group.
         console.log('The client ' + client.id + ' is closed.');
         if (this.getActiveClientsNumberByGroup(client.groupId) === 0) {
-          socketHandler.garbageCollection(client.groupId);
+          this.socketHandler.garbageCollection(client.groupId);
           console.log(client.groupId + ' group is removed.');
         }
-      }.bind(this));
-    }.bind(this));
-  },
+      });
+    });
+  }
 
-  sendMessageToGroup: function(id, message) {
-    for (var clientId in this.groups[id]) {
-      var readyState = this.groups[id][clientId].readyState;
+  sendMessageToGroup(id, message) {
+    for (let clientId in this.groups[id]) {
+      let readyState = this.groups[id][clientId].readyState;
       console.log('Client ' + clientId + ' readyState: ' + readyState);
 
       if (readyState === READY_STATE_OPEN) {
         this.groups[id][clientId].send(message);
       }
     }
-  },
+  }
 
-  broadcast: function(message) {
-    this.server.clients.forEach(function(client) {
+  broadcast(message) {
+    this.server.clients.forEach((client) => {
       client.send(message);
     });
-  },
+  }
 
-  generateHashId: function() {
+  generateHashId() {
     return new Hashids(uuid.v1()).encrypt(12345);
-  },
+  }
 
-  getActiveClientsNumberByGroup: function(groupId) {
-    var length = 0;
-    for (var clientId in this.groups[groupId]) {
+  getActiveClientsNumberByGroup(groupId) {
+    let length = 0;
+    for (let clientId in this.groups[groupId]) {
       if (this.groups[groupId][clientId].readyState === READY_STATE_OPEN) {
-        length += 1;
+        length++;
       }
     }
     return length;
   }
-};
+}
 
 module.exports = WebSocketServer;
